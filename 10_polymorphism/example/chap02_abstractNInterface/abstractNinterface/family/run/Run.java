63c9b5fa8d580b512f331923379ea5f4e4f1b02b
임연화(Im Yeon-HWA) <59764227+YeonHwa-IM@users.noreply.github.com>
2020-02-03 16:33:30 +0900

    Delete Run.java

--- 10_polymorphism/example/chap02_abstractNInterface/abstractNinterface/family/run/Run.java
+++
@@ -1,9 +0,0 @@
-package com.kh.example.chap02.abstractNinterface.family.run;
-
-import com.kh.example.chap02.abstractNinterface.family.controller.FamilyController;
-
-public class Run {
-	public static void main(String[] args) {
-		new FamilyController().method();
-	}
-}
